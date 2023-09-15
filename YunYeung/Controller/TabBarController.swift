e469b0bff743e126f85e3903b099136ce8e987b8
eguchi-ap <t.eguchi@alphapolis.co.jp>
2023-09-15 11:17:58 +0900

    ログイン画面実装した

--- YunYeung/Controller/TabBarController.swift
+++ YunYeung/Controller/TabBarController.swift
@@ -6,8 +6,21 @@
 //
 
 import UIKit
+import FirebaseAuth // 先頭でFirebaseAuthをimportしておく
+
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // currentUserがnilならログインしていない
+        if Auth.auth().currentUser == nil {
+            // ログインしていないときの処理
+            let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+            self.present(loginViewController!, animated: true, completion: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
