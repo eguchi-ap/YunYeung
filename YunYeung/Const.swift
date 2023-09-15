f0685a0879444c7cc49de90a7d0fe0fa008d8fb6
eguchi-ap <t.eguchi@alphapolis.co.jp>
2023-09-15 13:05:04 +0900

    データ投稿機能の実装

---
+++ YunYeung/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  YunYeung
+//
+//  Created by WEBSYSTEM-MAC39 on 2023/09/15.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
